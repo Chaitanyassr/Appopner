552963e98d550483d9bc50d24a4e6ff2ef7f5df8
Chaitanyassr <chaitanyas0101@gmail.com>
2020-08-04 14:55:56 +0530

    added a task fucntion

--- Javascript.js
+++ Javascript.js
@@ -2,8 +2,12 @@ function openApp() {
   var txt;
   if (confirm("Open the application")) {
     txt = "Your application is opening!";
+    Task();
   } else {
     txt = "You pressed Cancel!";
   }
   document.getElementById("demo").innerHTML = txt;
+}
+function Task(){
+	
 }
