b6181bd3e753d6a99a8f107cd8c53d505eb79db6
Chaitanyassr <chaitanyas0101@gmail.com>
2020-08-05 07:44:23 +0530

    added lines

--- batch.bat
+++ batch.bat
@@ -1,7 +1,3 @@
 echo off
-
-
 for /R "C:\wamp64" %%I in ("whiteboard*.exe") do if /I "%%~nxI" == "whiteboard.exe" "%%I" createdatabase closeimmediately
-
-
 exit
--- index.php
+++ index.php
@@ -1,16 +1,3 @@
-<!-- <!DOCTYPE html>
-<html>
-<head>
-	<title>appopner</title>
-
-</head>
-<body onload="openApp()">
-<p id="demo"></p>
-
-
-</body>
-</html>
- -->
 <?php 
 echo "string";
 system('start /b batch.bat'); 
